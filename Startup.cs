9533afe82ee54053fdcb283172402b34f99fd9b1
asachanxxx <cspl.itm@googlemail.com>
2017-06-15 20:37:38 +0530

    ssdasd
    First Commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeManager.Startup))]
+namespace HomeManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
